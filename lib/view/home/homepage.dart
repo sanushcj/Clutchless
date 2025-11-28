a1d35dbbf627b29c95690909b2bcf9324b1003d9
Sanush C J <sanushcj@gmail.com>
2025-11-28 13:43:30 +0530

    scratch

---
+++ lib/view/home/homepage.dart
@@ -0,0 +1,18 @@
+ import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+   
+return Scaffold();
+
+
+
+  }
+
+
+
+    
+ }
